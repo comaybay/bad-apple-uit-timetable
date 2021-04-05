54a4bdb8478055f1f8da285d3d625fe4127ebb2d
CMB <thaichibao@gmail.com>
2021-04-05 23:53:41 +0700

    simpler backend

--- bad_apple.py
+++ bad_apple.py
@@ -1,20 +1,9 @@
-import http.server
-import socketserver
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
 import os
 
-
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/script":
-            self.path = 'script.js'
-
-        elif self.path == "/data":
-            self.path = 'data.json'
-
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-
 # start chrome without cors
-os.system('start chrome daa.uit.edu.vn --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp')
+os.system('start chrome daa.uit.edu.vn --disable-web-security --user-data-dir=./chromeTemp')
 
-socketserver.TCPServer(("", 9999), MyHttpRequestHandler).serve_forever()
+server = TCPServer(("", 9999), SimpleHTTPRequestHandler)
+server.serve_forever()
