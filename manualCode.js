7da3bca960f38ed8718d046d5ce293fe158e42d6
CMB <thaichibao@gmail.com>
2021-04-05 23:54:14 +0700

    added gitignore, mp3 file and update manual code

--- manualCode.js
+++ manualCode.js
@@ -1,5 +1,5 @@
 //Copy and paste this into the developer tools console.
 let script = document.createElement("script");
-script.src = 'http://localhost:9999/script';
+script.src = 'http://localhost:9999/script.js';
 document.body.appendChild(script);
 
