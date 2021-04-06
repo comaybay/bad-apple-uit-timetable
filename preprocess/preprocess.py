d5b41145d2c485d216e3603fb01d017ad9eceef5
CMB <thaichibao@gmail.com>
2021-04-06 20:28:11 +0700

    update srt files to sync with the video

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -11,7 +11,7 @@ TRANSCRIPT_PATH_EN = os.path.join(SCRIPT_PATH, 'transcript_en.srt')
 TRANSCRIPT_PATH_JP = os.path.join(SCRIPT_PATH, 'transcript_jp.srt')
 TRANSCRIPT_PATH_ROMAJI = os.path.join(SCRIPT_PATH, 'transcript_romaji.srt')
 
-video.process(VIDEO_PATH, 'frames.json', VIDEO_SCALE)
+# video.process(VIDEO_PATH, 'frames.json', VIDEO_SCALE)
 transcript.process(TRANSCRIPT_PATH_EN, 'transcript_en.json')
 transcript.process(TRANSCRIPT_PATH_JP, 'transcript_jp.json')
 transcript.process(TRANSCRIPT_PATH_ROMAJI, 'transcript_romaji.json')
